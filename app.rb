c86d98ed1411f36696067748171ef86cb2d48f96
abchi <5568900+abchi@users.noreply.github.com>
2020-03-19 18:20:33 +0900

    feat: app.rbを追加

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require "bundler/setup"
 Bundler.require
+require "sinatra/reloader" if development?
+require "yaml"
+require "./converter.rb"
+
+title = "MTG ARENA DECK CONVERTER"
 
 get "/" do
-    "hello"
+    @title = title
+    @database = PG::connect($dbconf)
+    @input_list = ""
+    @output_list = ""
+    erb :mtga_deck_converter, :layout => :layout
+end
+
+post "/" do
+    @title = title
+    @database = PG::connect($dbconf)
+    @input_list = params[:input_list]
+    @output_list = text_conversion(read_deck_list(params[:input_list]), "ja")
+    erb :mtga_deck_converter, :layout => :layout
 end

d952331abe46f2d7da9afae51d7b8c0f067648e1
abchi <5568900+abchi@users.noreply.github.com>
2022-06-19 01:26:02 +0900

    style: フォーマッターの適用

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ require './converter.rb'
 title = 'MTG ARENA DECK CONVERTER'
 
 get '/' do
-    @title = title
-    @database = PG::connect($dbconf)
-    @deck_list = ''
-    erb :mtga_deck_converter, :layout => :layout
+  @title = title
+  @database = PG::connect($dbconf)
+  @deck_list = ''
+  erb :mtga_deck_converter, :layout => :layout
 end
 
 post '/' do
-    @title = title
-    @database = PG::connect($dbconf)
-    @deck_list = text_conversion(read_deck_list(params[:deck_list]), 'ja')
-    erb :mtga_deck_converter, :layout => :layout
+  @title = title
+  @database = PG::connect($dbconf)
+  @deck_list = text_conversion(read_deck_list(params[:deck_list]), 'ja')
+  erb :mtga_deck_converter, :layout => :layout
 end
